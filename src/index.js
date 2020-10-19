69187f56f2e26562fed6bea13753093aab573610
Jacob Briggs <jdbriggs81@gmail.com>
2020-10-19 14:30:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './App';
-import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
